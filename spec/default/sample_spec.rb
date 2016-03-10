15e6ee2b43f2e28a967885d6a5d7a57a93a58c86
yrros <yrros@hotmail.com>
2016-03-10 12:47:29 +0000

    Dev work ended... for now. Unit tests have a problem, serverSpec works but FW test still flawed

--- spec/default/sample_spec.rb
+++ spec/default/sample_spec.rb
@@ -1,16 +1,7 @@
 require 'spec_helper'
 
-describe package('redis-server'), :if => os[:family] == 'redhat' do
-  it { should be_installed }
-end
-
 describe package('redis-server'), :if => os[:family] == 'ubuntu' do
   it { should be_installed }
-end
-
-describe service('redis-server'), :if => os[:family] == 'redhat' do
-  it { should be_enabled }
-  it { should be_running }
 end
 
 describe service('redis-server'), :if => os[:family] == 'ubuntu' do
@@ -18,6 +9,12 @@ describe service('redis-server'), :if => os[:family] == 'ubuntu' do
   it { should be_running }
 end
 
+#check SSH is listening
+describe port(22) do
+  it { should be_listening }
+end
+
+#check Redis 6379 is listening
 describe port(6379) do
   it { should be_listening }
 end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,20 +7,22 @@
 require 'chefspec'
 
 describe 'cooking::default' do
-let(:chef_run) {
-   ChefSpec::Runner.new.converge(described_recipe)
-}
+  context 'When all attributes are default, on an unspecified platform' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new
+      runner.converge(described_recipe)
+    end
 
-# test that it creates a file /tmp/data.txt
-it 'Redis Input File Present' do
-chef_run = ChefSpec::Runner.new.converge(described_recipe)
-expect(chef_run).to create_file('/tmp/data.txt')
-end
+    # test that it creates a file /tmp/data.txt
+    it 'Redis Input File Present' do
+    chef_run = ChefSpec::Runner.new.converge(described_recipe)
+    expect(chef_run).to create_file('/tmp/data.txt')
+    end
 
-# test that the out file for redis has data in it /tmp/out_data.txt
-it 'Redis Output File Present' do
-chef_run = ChefSpec::Runner.new.converge(described_recipe)
-expect(chef_run).to create_file('/tmp/out_data.txt')
-end
-
+    # test that the out file for redis has data in it /tmp/out_data.txt
+    it 'Redis Output File Present' do
+    chef_run = ChefSpec::Runner.new.converge(described_recipe)
+    expect(chef_run).to create_file('/tmp/out_data.txt')
+    end
+  end
 end
